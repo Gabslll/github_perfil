38dfc005d0ac95d0c4603b3247d8a1cdb319a4a2
Gabriel Ramos <gabrielrammus42@gmail.com>
2024-04-21 21:38:57 -0300

    Adjusting message error 404

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -3,42 +3,58 @@ import { useEffect, useState } from "react";
 import styles from './ReposList.module.css';
 
 const ReposList = ({ nomeUsuario }) => {
-    const [repos, setRepos] = useState([])
+    const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [deuErro, setDeuErro] = useState(false);
 
     useEffect(() => {
-        setEstaCarregando(true)
+        setDeuErro(false);
+        setEstaCarregando(true);
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then( res => res.json() )
-        .then(resJson => {
-            setTimeout(() => {
+            .then(res => {
+                if (!res.ok) {
+                    if (res.status === 404) {
+                        throw new Error('Usuário não encontrado');
+                    } else {
+                        throw new Error('Erro desconhecido');
+                    }
+                }
+                return res.json();
+            })
+            .then(resJson => {
                 setEstaCarregando(false);
-                setRepos(resJson)
-            }, 3000)
-        })
-    }, [nomeUsuario])
+                setRepos(resJson);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar repositórios:', error.message);
+                setEstaCarregando(false); // Se ocorrer um erro, não está mais carregando
+                setDeuErro(true); // Configurar deuErro como verdadeiro
+            });
+    }, [nomeUsuario]);
+    
 
     return (
         <div className="container">
             {estaCarregando ? (
-            <h1>Carregando........</h1>
+                <h1>Carregando........</h1>
+            ) : deuErro ? (
+                <h1>Usuário não encontrado</h1>
             ) : (
-            <ul className={styles.list}>
-                {/* {repos.map(repositorio => ( */}
-                {repos.map(({ id, name, language, html_url }) => (
-                    <li className={styles.listItem} key={id}>
-                        <div className={styles.itemName}>
-                            <b>Nome: </b> {name}
-                        </div>
-                        <div className={styles.itemLanguage}>
-                            <b>Linguagem: </b> {language}
-                        </div>
-                        <div className={styles.itemLink}>
-                            <a target="_blank" href={html_url}>Visitar no Github</a>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+                <ul className={styles.list}>
+                    {repos.map(({ id, name, language, html_url }) => (
+                        <li className={styles.listItem} key={id}>
+                            <div className={styles.itemName}>
+                                <b>Nome: </b> {name}
+                            </div>
+                            <div className={styles.itemLanguage}>
+                                <b>Linguagem: </b> {language}
+                            </div>
+                            <div className={styles.itemLink}>
+                                <a target="_blank" href={html_url}>Visitar no Github</a>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             )}
         </div>
     )
